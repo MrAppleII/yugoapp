a6a06f906e1a8fd0569a06cc96a7d58473afc8ab
Jesus Garnica <jgarnica1@mail.sfsu.edu>
2018-05-16 13:17:53 -0700

    New Yugo Code
    Yugo redone in Xamarin to support iOS and Android

---
+++ Yugo/Yugo/AskQuestions.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Yugo
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class AskQuestions : ContentPage
+	{
+		public AskQuestions ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
